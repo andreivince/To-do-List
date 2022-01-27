bf9284d5405c423958dab8f0ff870bfff98543a0
Andrei <andreivince21@gmail.com>
2022-01-27 03:36:08 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const criarItem = () => {
+    const item = document.createElement('teste1') /*Colocando o java script para criar essa div*/
+    item.classList.add('teste1')
+    item.innerHTML = ´
+    <input type="checkbox" class="check1">
+    <h2>Teste da Lista</h2>
+    <input type="button" value="X" class="x1">
+        ´
+
+}
+
+
+
+
