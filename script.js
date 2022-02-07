3f0f67e8642462fab2a5824ce1ef3d28d30a7ba1
Andrei <andreivince21@gmail.com>
2022-02-06 21:03:26 -0300

    Adicionando Tarefa

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const criarItem = () => {
-    const item = document.createElement('teste1') /*Colocando o java script para criar essa div*/
-    item.classList.add('teste1')
-    item.innerHTML = ´
-    <input type="checkbox" class="check1">
-    <h2>Teste da Lista</h2>
-    <input type="button" value="X" class="x1">
-        ´
-
+const criarItem = (tarefa, status) => {
+    const item = document.createElement('teste1');
+    item.classList.add('teste1');
+    item.innerHTML = `
+        <input type="checkbox" class="check1" ${status}> 
+        <h2>${tarefa}</h2> 
+        <input type="button" value="X" class="x1">
+    `;  
+    document.getElementById('tarefa').appendChild(item);
 }
 
-
-
+//Ele acaba recriando o elemento, e esses ${} são status ou tarefa que seria os parametros
+//Não é preciso crair um ${status}
 
